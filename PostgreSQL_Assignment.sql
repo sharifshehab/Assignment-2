9938e85414f5f704e911552ed94a0b13d2f675c0
Shrafi Shehab <svshuvo696@gmail.com>
2025-05-24 21:02:19 +0600

    problem 1 solved

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,16 @@
 
+/* 
+
+*/
+
+SELECT * FROM rangers;
+SELECT * FROM species;
+SELECT * FROM sightings;
+
+
+
+-- Problem 1: Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+INSERT INTO rangers(ranger_id, name, region) VALUES(1, 'Derek Fox', 'Coastal Plains');
+
+
+
